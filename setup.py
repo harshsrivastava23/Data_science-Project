2f7f032ed07d3d951f0d6a38c8868c75890f6755
unknown <harshsrivastava270@gmail.com>
2025-04-06 20:29:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup (
+    name='mlproject',
+    version='0.0.1',
+    author='harsh',
+    author_email='harshsrivastava270@gmail.com',
+    packages=find_packages(),
+    install_requires= ['pandas','numpy','seaborn']
+)
